8e7da529be8fa68e63813e404721743092f9e694
mzd <daryabeygi@ginkgostreet.com>
2013-03-27 00:00:58 -0400

    add config file for templates

---
+++ tweet_templates.php
@@ -0,0 +1,16 @@
+<?php
+
+function get_template_config() {
+    return array(
+
+' @Jim_Jordan @repcohen @reptrentfranks @Randy_Forbes %s %s says support #aaronslaw, not harsher #CFAA',
+' @Jim_Jordan @repcohen @reptrentfranks @Randy_Forbes %s %s also says dont make #cfaa crazier: support #aaronslaw',
+' @Jim_Jordan @repcohen @reptrentfranks @Randy_Forbes #cfaa kills innovation. %s %s says back #aaronslaw',
+' @Jim_Jordan @repcohen @reptrentfranks @Randy_Forbes #cfaa is unjust. %s %s says support #aaronslaw',
+'@Raul_Labrador @RepRichmond @repbobbyscott @jasoninthehouse %s %s says support #aaronslaw, not harsher #CFAA',
+' @Raul_Labrador @RepRichmond @repbobbyscott @jasoninthehouse %s %s also says dont make #cfaa crazier: support #aaronslaw',
+' @Raul_Labrador @RepRichmond @repbobbyscott @jasoninthehouse #cfaa kills innovation. %s %s says back #aaronslaw',
+' @Raul_Labrador @RepRichmond @repbobbyscott @jasoninthehouse #cfaa is unjust. %s %s says support #aaronslaw',
+    );
+
+}
