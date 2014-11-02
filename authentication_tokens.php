1dd0129cf9a4c771940e60f9bb64566d3642a513
ginkgoMZD <mzd@ginkgostreet.com>
2014-11-01 22:59:00 -0400

    myriad changes from last implementation: move get_oauth to authentication_tokens add tpl_pointer increase interval and batch size respond to STOP command (file present)

---
+++ authentication_tokens.php
@@ -0,0 +1,11 @@
+<?php
+
+function get_oauth() {
+  return array(
+    'consumer key' => 'YMwWrqnEtKp4rdsWupZ20A',
+    'consumer secret' => 'QpKs2BM4x0RFNixHJuWBNuvMbLHg4DjI5jwIcjtpMg',
+    'oauth token' => '1305075690-VwDRm6XHunUdXKeHC9vCbeEoEJn61VboGATA9GJ',
+    'oauth secret' => '0zf1S9lNZdV8CDy40AVX0Ls82WAfY1yaBc4uBdiv0A',
+  );
+}
+
--- robo-tweet.php
+++ robo-tweet.php
@@ -1,4 +1,4 @@
-#!/usr/bin/php -q
+#!/usr/local/bin/php -q
 Robo-Tweeting
 <?php
 error_reporting(E_ALL);
@@ -7,9 +7,10 @@ include dirname(__FILE__).'/db-conf.php'; # configure database connection variab
 include dirname(__FILE__).'/lib-php-cli-io.php';
 include dirname(__FILE__).'/tweet.php';
 include dirname(__FILE__).'/tweet_templates.php';
+include dirname(__FILE__).'/authentication_tokens.php';
 
-$interval = 190; # seconds
-$number_of_petitioners_per_run = 5;
+$interval = 60; # 190 seconds
+$number_of_petitioners_per_run = 500;
 
 $get_signers_sql = "SELECT * 
 FROM anonymous_petitioners p LEFT JOIN last_tweet l on p.id = l.petitioner_key
@@ -18,15 +19,6 @@ ORDER BY l.last_tweeted ASC
 
 $log_tweet_sql = "INSERT INTO last_tweet (`petitioner_key`, `last_tweeted`, `tweet`) VALUES ('%s', NOW(), '%s')
 ON DUPLICATE KEY UPDATE `last_tweeted` = VALUES(`last_tweeted`), `tweet` = VALUES(`tweet`);";
-
-function get_oauth() {
-  return array(
-    'consumer key' => 'SQ05blEcp1AXpl2vWUhdg',
-    'consumer secret' => 'rTTouoiT3dXY71sOFt1ruCjhCCCN7jzPfQnvtIQ2TmA',
-    'oauth token' => '1179371964-f3QeH5BQfgUvK9150JWU73WJBNyuhCpfIDcQV5g',
-    'oauth secret' => 'GRR6UCMn2iBjjOFKgh6UFeqyfVopdWiQz2nBBnTRU',
-  );
-}
 
 ### END CONFIG ###
 
@@ -49,16 +41,27 @@ function num_templates() {
   return count($tweet_templates);
 }
 
-$tpl_pointer = 0;
+function tpl_pointer($zero = null) {
+	$file = dirname(__FILE__).'/tpl_pointer';
+	$pointer = 0;
+
+	if (file_exists($file)) {
+		$pointer = unserialize(file_get_contents($file));
+		$pointer = $pointer+1;
+		if ($zero === 0) $pointer = 0;
+	}
+	file_put_contents($file, serialize($pointer));
+	return $pointer;
+}
+
 function get_tpl($pntr = NULL) {
-  global $tpl_pointer;
   global $tweet_templates;
   $max = num_templates()-1;
+  $tpl_pointer = tpl_pointer();
+  $tpl_pointer = ($tpl_pointer > $max) ?  tpl_pointer(0) : $tpl_pointer;
 
   if (is_null($pntr)) {
-    # use the global pointer
-    if ($tpl_pointer > $max) $tpl_pointer = 0;
-    return $tweet_templates[$tpl_pointer++];
+    return $tweet_templates[$tpl_pointer];
   } else { #return a specific tpl
     while ($pntr > $max ) {
       #emulate infinite tpl's: shutup YAGNI
@@ -76,12 +79,12 @@ function get_tweet($signer) {
 
   global $tpl_pointer;
 
-  $tweet = sprintf(get_tpl(), $signer['first'], $signer['l-initial'], $signer['state']);
+  $tweet = sprintf(get_tpl(), $signer['first'], $signer['l-initial'], $signer['city'], $signer['state']);
 
   if ( strlen($tweet) > 140 ) {
     #try a different template:
     for ($n = 0; $n < num_templates(); $n++) {
-      $tweet = sprintf(get_tpl($n), $signer['first'], $signer['l-initial'], $signer['state']);
+      $tweet = sprintf(get_tpl($n), $signer['first'], $signer['l-initial'], $signer['city'], $signer['state']);
       if ( strlen($tweet) < 140 ) return $tweet;
     }
   }
@@ -94,6 +97,8 @@ function get_tweet($signer) {
 }
 
 while ($signer = mysqli_fetch_array($signers) ) {
+    if (file_exists(dirname(__FILE__).'/STOP')) die('STOP signal received');
+
     $tweet = get_tweet($signer);
 
     if (is_null($tweet)) {
@@ -102,6 +107,7 @@ while ($signer = mysqli_fetch_array($signers) ) {
     }
 
     # TWEET TWEET
+    $tweet_status = 999;
     $tweet_status = tweet($tweet, get_oauth());
     if ($tweet_status['http status code'] != 200) var_dump($tweet_status);
 
